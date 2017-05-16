3028d245e1ac2c36f0165fd7ee627221f7fb5b72
selu91 <cristea.marius.alexandru@gmail.com>
2017-05-16 14:26:08 +0300

    added grunt rsync.

---
+++ grunt/rsync.js
@@ -0,0 +1,33 @@
+//https://github.com/jedrichards/grunt-rsync
+module.exports = function (grunt, options) {
+    return {
+            options: {
+                args: ["--verbose"],
+                recursive: true
+            },
+            start: {
+                options: {
+                    src: ".maintenance",
+                    dest: process.env.DEMO_ROOT,
+                    host: process.env.DEMO_THEMEISLE_USER+"@"+process.env.DEMO_THEMEISLE_HOST,
+                    delete: true // Careful this option could cause data loss, read the docs!
+                }
+            },
+            prod: {
+                options: {
+                    src: "dist/",
+                    dest: process.env.DEMO_THEMEISLE_PATH,
+                    host: process.env.DEMO_THEMEISLE_USER+"@"+process.env.DEMO_THEMEISLE_HOST,
+                    delete: true // Careful this option could cause data loss, read the docs!
+                }
+            },
+            end: {
+                options: {
+                    src: ".maintenance",
+                    dest: process.env.DEMO_ROOT,
+                    host: process.env.DEMO_THEMEISLE_USER+"@"+process.env.DEMO_THEMEISLE_HOST,
+                    delete: true // Careful this option could cause data loss, read the docs!
+                }
+            }
+    }
+};
