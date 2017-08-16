fc74b093c31c9969901cc4fd7a407d198e47ea3c
selu91 <cristea.marius.alexandru@gmail.com>
2017-08-16 16:11:11 +0300

    Fixed path issues on copy.

--- grunt/copy.js
+++ grunt/copy.js
@@ -3,26 +3,36 @@
  */
 
 module.exports = function (grunt, options) {
-    return {
-        deploy: {
-            files: [{
-                src: ['**'],
-                dest: 'dist/',
-                filter: function (filepath) {
-                    var exclude = grunt.file.read('.distignore');
-                    var cwd = grunt.file.read('.distignore').split(/\r|\n/);
-                    cwd = cwd.filter(function (e) {
-                        return e
-                    });
-                    for (index in cwd) {
-                        var file = cwd[index];
-                        if (filepath.indexOf(file) === 0) {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-            }]
-        }
-    };
+	return {
+		deploy: {
+			files: [{
+				src: ['**'],
+				dest: 'dist/',
+				filter: function (filepath) {
+					var unixifyPath = function (filepath) {
+
+						var isWindows = process.platform === 'win32';
+						if (isWindows) {
+							return filepath.replace(/\\/g, '/');
+						} else {
+							return filepath;
+						}
+					};
+					filepath = unixifyPath(filepath);
+					var exclude = grunt.file.read('.distignore');
+					var cwd = grunt.file.read('.distignore').split(/\r|\n/);
+					cwd = cwd.filter(function (e) {
+						return e
+					});
+					for (index in cwd) {
+						var file = cwd[index];
+						if (filepath.indexOf(file) === 0) {
+							return false;
+						}
+					}
+					return true;
+				}
+			}]
+		}
+	};
 };
