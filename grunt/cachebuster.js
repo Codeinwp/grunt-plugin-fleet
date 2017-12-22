6148ae6f85405e7452c0bfded51d460105f1e863
selu91 <cristea.marius.alexandru@gmail.com>
2017-12-22 15:11:51 +0200

    Adds distignore for checksum generator. Adds only first level files for json.

--- grunt/cachebuster.js
+++ grunt/cachebuster.js
@@ -1,7 +1,33 @@
 // https://github.com/felthy/grunt-cachebuster
-module.exports = {
-    default: {
-        src: [ '<%= files.php %>', '<%= files.css %>', '<%= files.js %>', '<%= hash.exclude %>', '!' + '<%= hash.file_name %>' ],
-        dest: '<%= hash.file_name %>'
-    }
+module.exports = function (grunt, options) {
+	return {
+		default: {
+			src: ['*.php', '*.css', '*.js', '<%= hash.exclude %>'],
+			dest: '<%= hash.file_name %>',
+			filter: function (filepath) {
+				var unixifyPath = function (filepath) {
+
+					var isWindows = process.platform === 'win32';
+					if (isWindows) {
+						return filepath.replace(/\\/g, '/');
+					} else {
+						return filepath;
+					}
+				};
+				filepath = unixifyPath(filepath);
+				var exclude = grunt.file.read('.distignore');
+				var cwd = grunt.file.read('.distignore').split(/\r|\n/);
+				cwd = cwd.filter(function (e) {
+					return e
+				});
+				for (index in cwd) {
+					var file = cwd[index];
+					if (filepath.indexOf(file) === 0) {
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+	}
 };
